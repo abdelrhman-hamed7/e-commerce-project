67a98f16b2f2726177a286d9439edbd7b591b0cd
Reem Osama <reemosamasalih@gmail.com>
2026-06-12 19:29:30 +0200

    45

--- E-commerce_final_project_24144-2024/config.php
+++ E-commerce_final_project_24144-2024/config.php
@@ -1,33 +1,11 @@
 <?php
 
-// Get DATABASE_URL from Render
+echo "<h1>CONFIG TEST</h1>";
+
+echo "<pre>";
+print_r(PDO::getAvailableDrivers());
+
 $DATABASE_URL = getenv("DATABASE_URL");
+echo "<br><br>DATABASE_URL = " . ($DATABASE_URL ? "FOUND" : "NOT FOUND");
 
-if (!$DATABASE_URL) {
-    die("DATABASE_URL is not set");
-}
-
-// Parse URL
-$url = parse_url($DATABASE_URL);
-
-$host = $url["host"];
-$port = $url["port"] ?? 5432;
-$dbname = ltrim($url["path"], "/");
-$user = $url["user"];
-$pass = $url["pass"];
-
-// PostgreSQL DSN (IMPORTANT)
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-
-try {
-    $conn = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
-
-} catch (PDOException $e) {
-    die("POSTGRES ERROR: " . $e->getMessage());
-}
-
-
-?>
+exit;
